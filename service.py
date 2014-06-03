4d17ae73fb3f5a4b1482b18555c3b76f980b4e56
Carlos Correia <cmfccorreia@gmail.com>
2014-06-03 04:02:07 +0100

    problems na pasta temp RPI openelec
    Tirando o decode resolve este erro: http://xbmclogs.com/show.php?id=216294

--- service.py
+++ service.py
@@ -36,7 +36,7 @@ __language__   = __addon__.getLocalizedString
 __cwd__        = xbmc.translatePath(__addon__.getAddonInfo('path')).decode("utf-8")
 __profile__    = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode("utf-8")
 __resource__   = xbmc.translatePath(pjoin(__cwd__, 'resources', 'lib' ) ).decode("utf-8")
-__temp__       = xbmc.translatePath(pjoin(__profile__, 'temp')).decode("utf-8")
+__temp__       = xbmc.translatePath(pjoin(__profile__, 'temp'))
 
 sys.path.append (__resource__)
 
