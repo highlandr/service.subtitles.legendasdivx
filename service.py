18d07f91e44c253008bf40b689af44da19962c69
Connor McLeod <highlander@teknorage.com>
2014-06-17 23:42:49 +0100

    Version update to v0.1.1

--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Service LegendasDivx.com version 0.1.0
+# Service LegendasDivx.com version 0.1.1
 # Code based on Undertext (FRODO) service
 # Coded by HiGhLaNdR@OLDSCHOOL
 # Ported to Gotham by HiGhLaNdR@OLDSCHOOL
